0fee6c37d4c40cee22d81c4ffd6c7eed66249daa
FelipePeralta01 <fe.peraltamiranda@gmail.com>
2024-10-29 02:46:08 -0300

    Cambio del mapa
    Se cambia loadscript a app.js para mantener el mapa cargado siempre y se borra jsx sin uso por ahora

--- App/frontend/src/App.js
+++ App/frontend/src/App.js
@@ -1,35 +1,34 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import { LoadScript } from '@react-google-maps/api'; // Importa LoadScript
 
-import {BarraNavegacion } from './componentes/BarraNavegacion'
-import {Home } from './pages/Home'
-import {Foro} from './pages/Foro'
-import {Tienda } from './pages/Tienda'
-import {Sesion} from './pages/Sesion'
-import  PuntosVerdesW from './pages/trabajador/PuntosVerdesW'
-import { Footer } from './componentes/Footer'
+import { BarraNavegacion } from './componentes/BarraNavegacion';
+import { Home } from './pages/Home';
+import { Foro } from './pages/Foro';
+import { Tienda } from './pages/Tienda';
+import { Sesion } from './pages/Sesion';
+import PuntosVerdesW from './pages/trabajador/PuntosVerdesW';
+import { Footer } from './componentes/Footer';
 import { Reciclaje } from './pages/Reciclaje';
 
-
 function App() {
-  
   return (
-    <div>
-    <Routes>
-        <Route path='/' element={<BarraNavegacion/>}>
-          <Route path='foro' element={<Foro/>} />
-          <Route path='/' element={<Home/>} />
-          <Route path='tienda' element={<Tienda/>} />
-          <Route path='mapa' element={< PuntosVerdesW />}/>
-          <Route path='sesion' element={<Sesion/>} />
-          <Route path='/' element={<Footer/>}/>
-          <Route path='reciclaje' element={<Reciclaje/>}/>
-
-        </Route>    
-    </Routes>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyAKEX7Y7Xo0tSLxB5fSZGuRZwMlV4NwANY" libraries={["places"]}> {/* Asegúrate de agregar tu API key aquí */}
+      <div>
+        <BarraNavegacion />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='foro' element={<Foro />} />
+          <Route path='tienda' element={<Tienda />} />
+          <Route path='mapa' element={<PuntosVerdesW />} />
+          <Route path='sesion' element={<Sesion />} />
+          <Route path='reciclaje' element={<Reciclaje />} />
+        </Routes>
+        <Footer />
+      </div>
+    </LoadScript>
   );
 }
 
--- App/frontend/src/pages/trabajador/PuntosVerdesW.jsx
+++ App/frontend/src/pages/trabajador/PuntosVerdesW.jsx
@@ -1,16 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
-
-import {
-  GoogleMap,
-  LoadScript,
-  Autocomplete,
-  Marker,
-} from "@react-google-maps/api";
-
+import { GoogleMap, Autocomplete, Marker } from "@react-google-maps/api";
+import axios from 'axios';
 import "./PuntosVerdesW.css";
 
-import axios from 'axios';
-
+// Configuración de axios
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
@@ -19,23 +12,14 @@ const client = axios.create({
   baseURL: "http://localhost:8000"
 });
 
-
-const initialCenter = {
-  lat: -33.363579,
-  lng: -70.678193,
-};
+const initialCenter = { lat: -33.363579, lng: -70.678193 };
 
 function PuntosVerdesW() {
-
-  const [items, setItems] = useState([]); // Estado para almacenar los datos de las cajitas
-
+  const [items, setItems] = useState([]);
   const [map, setMap] = useState(null);
   const [marker, setMarker] = useState(null);
   const [searchLocation, setSearchLocation] = useState(null);
-
-  const [puntosVerdes, setPuntosVerdes] = useState([]);
   const [center, setCenter] = useState(initialCenter);
-  const [error, setError] = useState('');
 
   const autoCompleteRef = useRef(null);
   const inputRef = useRef(null);
@@ -43,11 +27,10 @@ function PuntosVerdesW() {
   const onLoad = (autocomplete) => {
     autoCompleteRef.current = autocomplete;
   };
-  // API ya tiene habilitada geolocalizacion, falta modificar codigo correctamente con el uso de geolocalizacion
 
   const onPlaceChanged = () => {
     const autocomplete = autoCompleteRef.current;
-    if (autocomplete !== null) {
+    if (autocomplete && autocomplete.getPlace()) {
       const place = autocomplete.getPlace();
       if (place.geometry && place.geometry.location) {
         const location = place.geometry.location;
@@ -56,8 +39,6 @@ function PuntosVerdesW() {
           lng: location.lng(),
           address: place.formatted_address || "Dirección no disponible",
         });
-      } else {
-        console.log("Sin información de la ubicación.");
       }
     }
   };
@@ -65,106 +46,63 @@ function PuntosVerdesW() {
   const handleAddMarker = () => {
     if (searchLocation) {
       setMarker(searchLocation);
-      setPuntosVerdes([...puntosVerdes, searchLocation]);
-      console.log("Punto verde guardado: ", searchLocation.address);
+      setItems((prevItems) => [...prevItems, searchLocation]);
     }
   };
 
-  const handleLocationClick = (punto) => {
-    setCenter({ lat: punto.lat, lng: punto.lng });
-    setMarker({ lat: punto.lat, lng: punto.lng });
-    map.panTo({ lat: punto.lat, lng: punto.lng });
+  const handleLocationClick = (location) => {
+    setCenter({ lat: location.lat, lng: location.lng });
+    setMarker({ lat: location.lat, lng: location.lng });
+    map.panTo({ lat: location.lat, lng: location.lng });
   };
 
-  const handleDelete = (index) => {
-    const confirmDelete = window.confirm(
-      "¿Estás seguro de que deseas eliminar este punto verde?"
-    );
-    if (confirmDelete) {
-      const updatedPuntosVerdes = puntosVerdes.filter((_, i) => i !== index);
-      setPuntosVerdes(updatedPuntosVerdes);
-    }
-  };
-
-
-  const fetchItems = async () => {
-    
-    try {
-      const response = await client.get('/api/PtoVerde/ptoverde/'); // URL de la API
-      setItems(response.data); // Guarda los datos en el estado
-
-    } catch (error) {
-      console.error("Error al obtener los datos:", error);
-    }
-  };
-
-  // Llama a la función fetchItems cuando el componente se monta
   useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await client.get('/api/PtoVerde/ptoverde/');
+        setItems(response.data);
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+      }
+    };
     fetchItems();
   }, []);
 
-
-  const handleAddressClick = async (ptoVerde) => {
-    try {
-      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json`, {
-        params: {
-          ptoVerde: ptoVerde.direccion,
-          key: 'AIzaSyAKEX7Y7Xo0tSLxB5fSZGuRZwMlV4NwANY', // Reemplaza con tu clave de API
-        },
-      });
-
-      if (response.data.status === 'OK') {
-        const { lat, lng } = response.data.results[0].geometry.location;
-        setMarker({ lat, lng });
-        map.panTo({ lat, lng }); // Mover el mapa a la nueva ubicación
-        setError('');
-      } else {
-        setError('No se encontró la dirección.');
-      }
-    } catch (err) {
-      setError('Error al obtener la dirección.');
-      console.error(err);
-    }
-  };
-
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAKEX7Y7Xo0tSLxB5fSZGuRZwMlV4NwANY"libraries={["places"]}
-    >
-      <div className="map-container">
-        {/* Mapa de Google */}
-        <div className="map-section">
-          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-            <input
-              type="text"
-              ref={inputRef}
-              placeholder="Ingresa dirección"
-              className="autocomplete-input"
-            />
-          </Autocomplete>
-          <button onClick={handleAddMarker} className="add-marker-button">
-            Agregar Punto Verde
-          </button>
-          <GoogleMap
-            mapContainerClassName="google-map"
-            center={center}
-            zoom={15}
-            onLoad={(map) => setMap(map)}
-          >
-            {marker && <Marker position={marker} />}
-          </GoogleMap>
-        </div>
-
-        {/* Lista de puntos verdes */}
-        <div className="list-section">
-          <h3>Puntos Verdes Guardados</h3>
-          <ul className="puntos-verdes-lista">
-              {items.map((ptoVerde) =>(
-                  <li key={ptoVerde.idPv} onClick={()=>handleAddressClick({ptoVerde})} >{ptoVerde.direccion} #{ptoVerde.nro} COMUNA {ptoVerde.nomComuna}</li>
-            ))}
-          </ul>
-        </div>
+    <div className="map-container">
+      <div className="map-section">
+        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Ingresa dirección"
+            className="autocomplete-input"
+          />
+        </Autocomplete>
+        <button onClick={handleAddMarker} className="add-marker-button">
+          Agregar Punto Verde
+        </button>
+        <GoogleMap
+          mapContainerClassName="google-map"
+          center={center}
+          zoom={15}
+          onLoad={(mapInstance) => setMap(mapInstance)}
+        >
+          {marker && <Marker position={marker} />}
+        </GoogleMap>
       </div>
-    </LoadScript>
+      {/*Lista de direcciones*/}
+      <div className="list-section">
+        <h3>Puntos Verdes Guardados</h3>
+        <ul className="puntos-verdes-lista">
+          {items.map((item, index) => (
+            <li key={index} onClick={() => handleLocationClick(item)}>
+              {item.address || item.direccion}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
